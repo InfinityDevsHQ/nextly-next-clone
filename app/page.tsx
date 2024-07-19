4cb05305f0a914c7f11812832f389a2df04c2efc
sheraz4196 <sheraztoor321@gmail.com>
2024-07-19 16:45:45 +0500

    cleanup: removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Trusted from "./components/Trusted";
 import Benefits from "./components/Benefits";
@@ -8,7 +7,6 @@ import Testimonials from "./components/Testimonials";
 import Faq from "./components/Faq";
 import Cta from "./components/cta";
 import WatchVideo from "./components/WatchVideo";
-import Footer from "./components/Footer";
 
 export default function Home() {
   return (
