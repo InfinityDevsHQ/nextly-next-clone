21ab884b78e8a56ecf6451572a70cc10eabc0f00
sheraz4196 <sheraztoor321@gmail.com>
2024-07-19 16:39:31 +0500

    chore: moved header and footer to the layout so if we have multiple pages they will show everywhere

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Trusted from "./components/Trusted"
-import Benefits from "./components/Benefits"
-import Highlight1 from "./components/Highlight"
-import Highlight2 from "./components/Highlight2"
-import Testimonials from "./components/Testimonials"
-import Faq from "./components/Faq"
-import Cta from "./components/cta"
-import WatchVideo from "./components/WatchVideo"
-import Footer from "./components/Footer"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Trusted from "./components/Trusted";
+import Benefits from "./components/Benefits";
+import Highlight1 from "./components/Highlight";
+import Highlight2 from "./components/Highlight2";
+import Testimonials from "./components/Testimonials";
+import Faq from "./components/Faq";
+import Cta from "./components/cta";
+import WatchVideo from "./components/WatchVideo";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="max-w-7xl mx-auto">
-      <Navbar />
+    <>
       <Hero />
       <Trusted />
       <Benefits />
@@ -23,7 +22,6 @@ export default function Home() {
       <Testimonials />
       <Faq />
       <Cta />
-      <Footer />
-    </div>
-  )
+    </>
+  );
 }
