48ef47db3ec18be495252d93cd7836ba4e68c9e6
sheraz4196 <sheraztoor321@gmail.com>
2024-07-19 18:10:41 +0500

    chore: replaced image with svg

--- app/components/general/Navbar.jsx
+++ app/components/general/Navbar.jsx
@@ -14,13 +14,19 @@ function Navbar() {
           <a href="/">
             <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
               <span>
-                <Image
-                  alt="N"
-                  width="32"
-                  height="32"
-                  className="w-8"
-                  src="/logo.svg"
-                />
+                <svg
+                  width="44"
+                  height="44"
+                  viewBox="0 0 44 44"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect width="44" height="44" rx="6" fill="#4F46E5" />
+                  <path
+                    d="M31.3662 34H25.7427L17.4282 19.4014V34H11.7725V10.5391H17.4282L25.7266 25.1377V10.5391H31.3662V34Z"
+                    fill="white"
+                  />
+                </svg>
               </span>
               <span>Nextly</span>
             </span>
