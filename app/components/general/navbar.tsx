dd20c7e2fe46b173e62df6144ffaa1e8f72b6d7b
sheraz4196 <sheraztoor321@gmail.com>
2024-07-22 11:20:18 +0500

    chore: separate component for logo svg

--- app/components/general/navbar.tsx
+++ app/components/general/navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import ModesToggler from "./modes-toggler";
 import { NavLinks } from "@/constants";
 import MobileHeader from "./mobile-header";
+import NextlySvg from "../svgs/nextly-svg";
 
 function Navbar() {
   return (
@@ -12,19 +13,7 @@ function Navbar() {
           <Link href="/">
             <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
               <span>
-                <svg
-                  width="44"
-                  height="44"
-                  viewBox="0 0 44 44"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <rect width="44" height="44" rx="6" fill="#4F46E5" />
-                  <path
-                    d="M31.3662 34H25.7427L17.4282 19.4014V34H11.7725V10.5391H17.4282L25.7266 25.1377V10.5391H31.3662V34Z"
-                    fill="white"
-                  />
-                </svg>
+                <NextlySvg />
               </span>
               <span>Nextly</span>
             </span>
